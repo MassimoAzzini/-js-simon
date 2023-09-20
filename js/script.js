893be6938289279006679e8a238bffd70b7aed0c
MassimoAzzini <massimo.azzini.web@gmail.com>
2023-09-20 21:21:51 +0200

    [edit]: JS to fix

--- js/script.js
+++ js/script.js
@@ -1,57 +1,72 @@
-const randomBox = document.getElementById('n-random')
-const userBox = document.getElementById('n-user')
-const checkBox = document.getElementById('check')
+const randomContainer = document.querySelector('.n-random')
+const userBox = document.querySelector('.n-user')
+const checkBox = document.querySelector('.cont-check')
 const btn = document.querySelector('button')
 
-console.log (randomBox)
+
 
 const randomNumber = [];
 const userNumber = [];
+const comparate = [];
 
 
-btn.addEventListener('click' function(){
-
+btn.addEventListener('click', function(){
+  
   createUniqueBombNumber(1, 10);
   
   for(let i = 0; i < randomNumber.length; i++){
-    printInPage(randomBox, randomNumber[i]);
+    printInPage(randomContainer, randomNumber[i], 'random');
   }
+  
   
   setTimeout(function(){
     
-    randomBox
+    // -----DA SISTEMARE----
+    const randomBox = document.querySelectorAll('.random');
+    for(let i = 0; i < NodeList.length; i++){
+      randomBox[i].innerHTML = '';
+
+    } 
+
+    console.log(randomBox)
     
-
-
+    
+    
+    
   }, 5000);
-  
-  
 
-  while(userNumber.length < 5){
+  setTimeout(function(){
+    
+    
+    
+    while(userNumber.length < 5){
+    
+      const numberPrompt = parseInt(prompt ('Inserisci i numeri che hai visto nello stesso ordine'))
+    
+      userNumber.push(numberPrompt)
+      console.log(userNumber)
+    
+    }
+    
+    for(let i = 0; i < userNumber.length; i++){
+      printInPage(userBox, userNumber[i], 'user');
+    }
+    
+    
+    
+    
+  }, 6000);
   
-    const numberPrompt = parseInt(prompt ('Inserisci i numeri che hai visto nello stesso ordine'))
   
-    userNumber.push(numberPrompt)
-    console.log(userNumber)
-  
-  }
-  
-  
-  
-  for(let i = 0; i < userNumber.length; i++){
-    printInPage(userBox, userNumber[i]);
-  }
-
 });
-
 
 
 
 // Funzione per stampare in pagina
 
-function printInPage(element, text){
+function printInPage(element, text, classEl){
   const newBox = document.createElement('div');
-  newBox.className = ('box');
+  newBox.classList.add(classEl);
   newBox.textContent = text;
   element.append(newBox);
 }
